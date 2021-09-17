eb5b49ece2f310a6762be746cd3bf2e089756b11
Nurbek Kuspanov <nurbek@inbox.ru>
2021-09-17 16:53:09 +0400

    wip

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import time
-def gudok (i = 0.1, chastota = 1000):
+def gudok (i = 0.1, chastota = 1000): # функция воспроизведения звука
     print ("\a", end = "\r")
     time.sleep (i)
 
-content = '---. . .-.. --- .-- . -.- / .-- / --- .--. .- ... -. --- ... - ..' 
+soobchenie_1 = '---. . .-.. --- .-- . -.- / .-- / --- .--. .- ... -. --- ... - ..' 
 
 azbukaMorze = {'а': '.-',
                'б': '-...',
@@ -47,15 +47,33 @@ azbukaMorze = {'а': '.-',
 
 adres = 'http://192.168.24.173:8080'
 
-def communication(adres):
-    response = requests.get(adres)
-    if response.status_code == 200:
+def communication(adres): # функция для проверки связи с роботом
+    r = requests.get(adres)
+    if r.status_code == 200:
         print('Связь с роботом установлена!')
     else:
         print('Нет связи с роботом')    
 
 #communication(adres)
-# for word in content:
+
+
+soobchenie_2='СОС СОС СОС' 
+soobchenie_2=soobchenie_2.lower()
+
+x=soobchenie_2[0]
+xx=soobchenie_2[1]
+xxx=soobchenie_2[2]
+probel1=soobchenie_2[3]
+y=soobchenie_2[4]
+yy=soobchenie_2[5]
+yyy=soobchenie_2[6]
+probel2=soobchenie_2[7]
+z=soobchenie_2[8]
+zz=soobchenie_2[9]
+zzz=soobchenie_2[10]
+morz=azbukaMorze[x]+azbukaMorze[xx]+azbukaMorze[xxx]
+print(morz)
+# for word in soobchenie_1:
 #     for symbol in word:
 #         if symbol == '.':
 #             gudok(i = 0.1)
@@ -63,13 +81,15 @@ def communication(adres):
 #             gudok(i = 0.7)
 #         elif symbol == '/':
 #             time.sleep(1)
-
-
-def robot(adres, m):
-    response = requests.post(adres,m)
-    if response.status_code == 200:
-        print('Команда принята')
+def robot(a, m): # функция для посыла сообщения роботу
+    r = requests.post(a,m.encode('utf-8'))
+    if r.status_code == 200:
+        print('Команда принята.')
+        time.sleep(1)
         print('Бегу к вам!')
     else:
-        print('Команда не принята! Продолжаю выполнять прежнюю инструкцию.')
-robot(adres, 'soss')            
+        print('Команда не принята. Продолжаю выполнять прежнюю инструкцию.')
+robot(adres,soobchenie_2)
+
+
+#'---. . .-.. --- .-- . -.- / .-- / --- .--. .- ... -. --- ... - ..'
