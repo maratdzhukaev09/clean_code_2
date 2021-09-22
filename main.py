ff1125ad0ffdf38c190881b607a2fc9f9cf2d071
Nurbek Kuspanov <nurbek@inbox.ru>
2021-09-22 16:46:56 +0400

    modernized main

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
 import time
-import sys
 import os
 import pygame
-from sound import playsound,play_morse_sound
-from sound import FREQ,BITSIZE,CHANNELS,BUFFER,FRAMERATE
-
+from alive_progress import alive_bar
+FREQ = 44100
+BITSIZE = -16
+CHANNELS = 2
+BUFFER = 1024
+FRAMERATE = 60
+MORSE_SOUND = {
+    '.': 'dot.ogg',
+    '-': 'dash.ogg',
+    '|': 'long_silence.ogg',
+}
 pygame.init ()
 os.system ('cls||clear')
 pygame.mixer.init (FREQ,BITSIZE,CHANNELS,BUFFER)
-
 azbukaMorze = {'а': '.-',
                'б': '-...',
                'в': '.--',
@@ -64,26 +70,8 @@ azbukaMorze = {'а': '.-',
                ')': '-.--.- ',
                ' ': '|',
                '_': '..--.- '}
-
-
-
-
-
-adres='http://192.168.24.173:8080'
-
-def communication(adres):
-    r = requests.get(adres)
-    if r.status_code == 200:
-        print('Связь с роботом установлена!')
-    else:
-        print('Нет связи с роботом')    
-    print()
-communication(adres)
-
-
 soobchenie='СОС СОС СОС' 
 soobchenie=soobchenie.lower()
-
 x=soobchenie[0]
 xx=soobchenie[1]
 xxx=soobchenie[2]
@@ -99,7 +87,228 @@ morz=azbukaMorze[x]+azbukaMorze[xx]+azbukaMorze[xxx]+\
     azbukaMorze[probel1]+azbukaMorze[y]+azbukaMorze[yy]+\
     azbukaMorze[yyy]+azbukaMorze[probel2]+azbukaMorze[z]+\
     azbukaMorze[zz]+azbukaMorze[zzz]        
-
+adres='http://192.168.24.173:8080'
+def communication(adres):
+    r = requests.get(adres)
+    if r.status_code == 200:
+        print('Связь с роботом установлена!')
+    else:
+        print('Нет связи с роботом')    
+    print()
+communication(adres)
+def playsound(soundfile):
+    sound = pygame.mixer.Sound(soundfile)
+    clock = pygame.time.Clock()
+    sound.play()
+    while pygame.mixer.get_busy():
+        clock.tick(FRAMERATE)
+def play_morse_sound(morz):
+    print()
+    with alive_bar(len(morz), bar = 'brackets', spinner = 'dots_waves2') as bar:
+        if morz[0] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[0] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[0] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[1] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[1] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[1] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[2] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[2] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[2] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[3] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[3] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[3] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[4] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[4] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[4] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[5] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[5] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[5] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()  
+        if morz[6] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[6] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[6] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[7] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[7] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[7] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[8] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[8] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[8] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[9] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[9] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[9] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()   
+        if morz[10] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[10] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[10] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[11] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[11] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[11] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[12] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[12] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[12] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[13] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[13] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[13] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[14] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[14] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[14] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[15] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[15] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[15] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[16] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[16] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[16] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[17] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[17] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[17] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[18] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[18] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[18] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[19] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[19] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[19] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[20] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[20] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[20] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[21] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[21] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[21] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[22] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[22] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[22] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[23] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[23] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[23] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[24] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[24] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[24] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[25] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[25] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[25] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[26] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[26] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[26] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[27] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[27] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[27] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+        if morz[28] == '.':
+            playsound(pygame.mixer.Sound('dot.ogg'))
+        elif morz[28] == '-':
+            playsound(pygame.mixer.Sound('dash.ogg'))
+        elif morz[28] == '|':
+            playsound(pygame.mixer.Sound('long_silence.ogg'))
+        bar()
+    print()
 def to_robot(a, m):
     print('Отправка сообщения роботу...')
     play_morse_sound(m)
@@ -110,7 +319,6 @@ def to_robot(a, m):
         print('Бегу к вам!')
     else:
         print('Команда не принята. Продолжаю выполнять прежнюю инструкцию.')
-
 to_robot(adres,morz)
 
 
